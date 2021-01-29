bb69e6c6d9ec7b83afc2803d0bbd3b24d97ec789
Student <ryanherren48@gmail.com>
2021-01-28 21:25:21 -0600

    updated many things, everything works except for the for loop for assigning sinks/heights

--- sandpile.c
+++ sandpile.c
@@ -3,30 +3,48 @@
 #include <stdlib.h>
 #include <string.h>
 int main(int argc, char *argv[]){
+  //grid elements- x axis, y axis, height
   int x,y,h;
-  int place = 3;
-  int grid[5][5];
+  //incrementors
+  int i, j, k, l, m, n;
+  //fps variable
+  int fps;
+  /**place will start iterations through cmdline arguments at place 1(right after the name)
+     as long as there is no --fps tag(see line 15)*/
+  int place = 1;
+  //custom size of grid for testing smaller grids
+  int gridSize = 5;
+  int grid[gridSize][gridSize];
 
-  if(argv[1] == "--fps"){
-    place = 1;
+  //initialize grid
+  for(l = 0; l < gridSize; l++){
+    for(m = 0; m < gridSize; m++){
+      grid[i][j] = 0;
+    }
   }
 
-  //fellas
+  if(argc > 1 && strcmp(argv[1], "--fps")){
+    place = 3;
+    fps = atoi(argv[2]);
+  }
 
   for(int i = place; i < argc; i+=3){
-    sscanf(argv[i], "%d", &y);
+    /**sscanf(argv[i], "%d", &y);
     sscanf(argv[i+1], "%d", &x);
-    sscanf(argv[i+2], "%d", &h);
+    sscanf(argv[i+2], "%d", &h);*/
+    y = atoi(argv[i]);
+    x = atoi(argv[i+1]);
+    h = atoi(argv[i+2]);
 
-    if(h<0){
-      grid[x][y] = 4;
+    if(h<0 && x != 11 && y != 11){
+      grid[x][y] = "#";
     }else{
       grid[x][y] = h;
     }
   }
 
-  for(int k = 0; k < 5; k++){
-    for(int j = 0; j < 5; j++){
+  for(int k = 0; k < gridSize; k++){
+    for(int j = 0; j < gridSize; j++){
       printf("%d", grid[k][j]);
     }
     printf("\n");
@@ -35,5 +53,6 @@ int main(int argc, char *argv[]){
   return 0;
   
 }
+
 
 
