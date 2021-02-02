c2107671721055b3ef6afff7427dbac6558dab93
Student <ryanherren48@gmail.com>
2021-02-01 23:17:39 -0600

    full functionality except for FPS, further testing required

--- sandpile.c
+++ sandpile.c
@@ -2,35 +2,36 @@
 #include <string.h>
 #include <stdlib.h>
 
-void printGrid(int grid[5][5]);
+int grid[23][23];
 
-void addNeighbors(int grid[5][5], int x, int y);
+void printGrid();
 
-void addSand(int grid[5][5], int x, int y);
+void addNeighbors(int x, int y);
+
+void addSand(int x, int y);
 
 int main(int argc, char *argv[]){
   //grid elements- x axis, y axis, height
   int x,y,h;
 
   //variables used to pass in as parameters for add sand
+  //starts at the center for all non-recursive calls
   int ux = 11, uy = 11;
   
   //incrementors
   int p, q, i, k, j;
-  
-
 
   /**place will start iterations through cmdline arguments at place 1(right after the name)
      as long as there is no --fps tag(see line 15)*/
   int place = 1;
   //custom size of grid for testing smaller grids
-  int gridSize = 5;
-  int grid[gridSize][gridSize];
+  
   
   int fps = 30;
+  
   //initialize the grid
-  for(int p = 0; p < 5; p++){
-    for(int q = 0; q < 5; q++){
+  for(int p = 0; p < 23; p++){
+    for(int q = 0; q < 23; q++){
       grid[p][q] = 0;
     }
   }
@@ -54,55 +55,45 @@ int main(int argc, char *argv[]){
       grid[x][y] = h;
     }
   }
-
-  
-
-  
- 
+  int z = 0;
    while(1){
     /*
       1. print grid
       2. add sand
       3. repeat
      */
-     printGrid(grid);
-     addSand(grid, ux, uy);
+     printGrid();
+     addSand(ux, uy);
+     z++;
   }
-
-
-  
-
   return 0;
-
-  
 }
 
-
- void addSand(int grid[5][5], int x, int y){
+void addSand(int x, int y){
     grid[x][y]++;
     if(grid[x][y] > 8){
 	grid[x][y] = 0;
-	addNeighbors(grid, x, y);
+	addNeighbors(x, y);
     }
   }
 
-  void addNeighbors(int grid[5][5], int x, int y){
-    for(int i = x -1; i < x + 1; i++){
-      for(int j = y - 1; j < y + 1; j++){
-	if(i < 5 && i >= 0 && j < 5 && j >= 0){
-	  grid[i][j]++;
+  void addNeighbors(int x, int y){
+    for(int i = x - 1; i <= x + 1; i++){
+      for(int j = y - 1; j <= y + 1; j++){
+	if(i < 23  && i >= 0 && j < 23 && j >= 0 && grid[i][j] != -1){
+	  grid[i][j] = grid[i][j] + 1;
 	  if(grid[i][j] > 8){
-	    addSand(grid, i, j);
-	  }
+	    addSand(i, j);
 	}
       }
     }
    }
+  }
 
-void printGrid(int grid[5][5]){
+void printGrid(){
   int i, j;
-  for(i = 0; i < 5; i++){
-    for(j = 0; j < 5; j++){
+  for(i = 0; i < 23; i++){
+    for(j = 0; j < 23; j++){
       if(grid[i][j] < 0){
 	printf("#");
       }else{
@@ -113,7 +104,7 @@ void printGrid(int grid[5][5]){
   }
 
   printf("\n");
-    
+  
 }
 
 
